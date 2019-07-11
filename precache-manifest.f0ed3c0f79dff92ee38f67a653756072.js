1070e5f8e5f58f93e700802a68f572b93cbcaa0b
--replace-all <akhbharti@gmail.com>
2019-07-11 20:35:30 +0530

    Updates

---
+++ precache-manifest.f0ed3c0f79dff92ee38f67a653756072.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651ab9692804fe74f1075df2674ae43b",
+    "url": "/adjetter-assignment/index.html"
+  },
+  {
+    "revision": "b465c03488245d917b8f",
+    "url": "/adjetter-assignment/static/css/main.0a29510e.chunk.css"
+  },
+  {
+    "revision": "51d23f4a28b9990cfd0a",
+    "url": "/adjetter-assignment/static/js/2.adf1fb2c.chunk.js"
+  },
+  {
+    "revision": "b465c03488245d917b8f",
+    "url": "/adjetter-assignment/static/js/main.50341fab.chunk.js"
+  },
+  {
+    "revision": "003e3f93e7f0a94d510b",
+    "url": "/adjetter-assignment/static/js/runtime~main.f8080a7f.js"
+  }
+]);
